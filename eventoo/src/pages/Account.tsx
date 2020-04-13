7fd39d89ff954cade3dcce026a043f369b2b7e65
Verthon <krzysiekksordyl@gmail.com>
2020-04-13 15:54:34 +0200

    feat: Added new basic layout for the Account Page.

--- eventoo/src/pages/Account.tsx
+++ eventoo/src/pages/Account.tsx
@@ -6,16 +6,18 @@ import {
   IonToolbar,
   IonLoading,
 } from '@ionic/react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
-import { db, auth, doSignOut } from '../firebase/firebase'
+import { doSignOut } from '../firebase/firebase'
 import { logout } from '../reducers/auth'
 import useAuthUser from '../hooks/useAuthUser'
-//import EventItem from '../components/EventItem'
+import EventItem from '../components/EventItem'
+import { EventType } from '../types/events'
+import {fetchUserEvents} from '../reducers/events'
 
 const Account: React.FC = (props: any) => {
-  const dispatch = useDispatch()
+  const dispatch: any = useDispatch()
   const currentUser = useAuthUser()
   console.log('Auth user object in Account useEffect', currentUser)
   const [events, setEvents] = useState([])
@@ -33,6 +35,20 @@ const Account: React.FC = (props: any) => {
     }
   }
 
+  useEffect(() => {
+    dispatch(fetchUserEvents(currentUser.uid))
+      .then((result: any) => {
+        console.log('fetchAllEvents result', result)
+        setDataFetched(true)
+        setEvents(result.payload)
+      })
+      .catch(() => {
+        console.log('Error while fetching the events')
+      })
+  }, [currentUser.uid, dispatch])
+
+  let currentEvents = useSelector((state: any) => state.events.userEvents)
+  console.log('current Events in Account', currentEvents)
   return (
     <IonPage>
       <IonHeader>
@@ -41,36 +57,96 @@ const Account: React.FC = (props: any) => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <IonLoading
-          isOpen={!isDataFetched}
-          onDidDismiss={() => setSpinner(false)}
-          message={'Please wait...'}
-          spinner="bubbles"
-          duration={500}
-        />
-        <p>KEK</p>
-        {/* <Avatar src={authUser ? authUser.photoURL : null} />
-        <Name>{authUser ? authUser.displayName : null}</Name> */}
+        <Container>
+          <IonLoading
+            isOpen={!isDataFetched}
+            onDidDismiss={() => setSpinner(false)}
+            message={'Please wait...'}
+            spinner="bubbles"
+            duration={500}
+          />
+          <Header>
+            <Avatar src={currentUser ? currentUser.avatar : null} />
+            <MetaContainer>
+              <Name>{currentUser ? currentUser.name : null}</Name>
+              <Email>{currentUser ? currentUser.email : null}</Email>
+            </MetaContainer>
+          </Header>
+          <Title>Your events</Title>
+          <EventsContainer>
+            {currentEvents
+              ? currentEvents.map((event: EventType, id: number) => {
+                  return (
+                    <EventItem
+                      key={id}
+                      docId={event.docId}
+                      eventId={event.eventId}
+                      title={event.title}
+                      localization={event.localization}
+                      address={event.address}
+                      host={event.host}
+                      date={event.date}
+                      day={event.day}
+                      hour={event.hour}
+                      description={event.description}
+                      category={event.category}
+                      featuredImage={event.featuredImage}
+                    />
+                  )
+                })
+              : null}
+          </EventsContainer>
 
-        <Button onClick={() => signOut()}>logout</Button>
+          <Button onClick={() => signOut()}>logout</Button>
+        </Container>
       </IonContent>
     </IonPage>
   )
 }
 
-const Avatar = styled.img`
-  border: 1px solid var(--ionic-color-primary);
-  border-radius: 50%;
-  max-width: 75px;
+const Title = styled.h2`
+  font-size: 1.25rem;
+  font-weight: 600;
 `
 
-const Name = styled.p`
-  margin: 1rem;
-  font-size: 2.5rem;
+const Container = styled.div`
+  padding: 1rem;
+`
+
+const Header = styled.header`
+  margin: 1rem 0 3rem 0;
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+`
+
+const Avatar = styled.img`
+  display: block;
+  border-radius: 50%;
+  width: 70px;
+  height: 70px;
+`
+
+const MetaContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 0 0 1.25rem;
+`
+
+const Name = styled.h2`
+  margin: 0;
+  font-size: 1.5rem;
   font-weight: bold;
 `
 
-const Email = styled.p``
+const Email = styled.p`
+  color: hsl(203, 13%, 44%);
+  margin: 0;
+`
+
+const EventsContainer = styled.div`
+  margin: 1rem 0;
+`
 
 const Button = styled.button`
   display: flex;
--- eventoo/src/reducers/events.ts
+++ eventoo/src/reducers/events.ts
@@ -7,6 +7,7 @@ export interface IEventsState {
   error: string | null,
   events: Array<EventType>,
   allEvents: Array<EventType>
+  userEvents: Array<EventType>
 }
 
 export const fetchAllEvents = createAsyncThunk('events/fetchAllEvents', async () => {
@@ -26,11 +27,30 @@ export const fetchAllEvents = createAsyncThunk('events/fetchAllEvents', async ()
   }
 })
 
+export const fetchUserEvents = createAsyncThunk('events/fetchUserEvents', async(uid) => {
+  try {
+    const querySnapshot = await db.collection('events')
+      .where("uid", "==", uid)
+      .get()
+    const events: Array<EventType> = []
+    querySnapshot.docs.forEach((doc: any) => {
+      const data = doc.data()
+      data.docId = doc.id
+      events.push(data)
+    })
+    return events
+  }
+  catch (err) {
+    console.log('error occurred while fetching events inside of eventsSlice', err)
+  }
+})
+
 const initialState: IEventsState = {
   loading: 'idle',
   error: null,
   events: [],
-  allEvents: []
+  allEvents: [],
+  userEvents: []
 }
 
 export const eventsSlice = createSlice({
@@ -46,7 +66,7 @@ export const eventsSlice = createSlice({
     filterEventsByCategory: (state: IEventsState, action: any): any => {
       console.log(action.payload)
       if (action.payload === "All") {
-        state.events = state.allEvents.map((event: EventType) => event);
+        state.events = [...state.allEvents]
         console.log('state.events after filter', state.events)
       }
       state.events = state.allEvents.filter((event: EventType) => event.category === action.payload)
@@ -60,6 +80,9 @@ export const eventsSlice = createSlice({
     builder.addCase(fetchAllEvents.fulfilled, (state: IEventsState, action) => {
       state.events = [...action.payload]
       state.allEvents = [...action.payload]
+    })
+    builder.addCase(fetchUserEvents.fulfilled, (state: IEventsState, action) => {
+      state.userEvents = [...action.payload]
     })
   }
 })
