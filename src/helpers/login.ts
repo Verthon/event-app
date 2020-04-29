8b057caa612f6ec7e74176051186aaf2839f08f4
Verthon <krzysiekksordyl@gmail.com>
2020-04-29 19:49:59 +0200

    refactor: login.ts file now holds the all login logic.

---
+++ src/helpers/login.ts
@@ -0,0 +1,44 @@
+import {
+  doSignInWithGoogle,
+  doSignInWithFacebook,
+  redirectResultGoogle,
+  doSignWithGoogleCredentials,
+  redirectResultFacebook,
+  doSignWithFacebookCredentials,
+} from '../firebase/firebase'
+
+import { ACCOUNT } from '../constants/routes'
+
+export const loginWithSocial = (provider: string, history: any) => {
+  if (provider === 'Google') {
+    doSignInWithGoogle()
+      .then(() => redirectResultGoogle())
+      .then((result: any) => {
+        const token = result.credential.accessToken
+        doSignWithGoogleCredentials(token)
+        history.push(ACCOUNT)
+      })
+      .catch((error: any) => {
+        console.log(
+          `Error occurred while signing in using ${provider} provider.`,
+          error
+        )
+        alert(`Error while trying to login with Google ==> ${error.message}`)
+      })
+  }
+  if (provider === 'Facebook') {
+    doSignInWithFacebook()
+      .then(() => redirectResultFacebook())
+      .then((result: any) => {
+        const token = result.credential.accessToken
+        doSignWithFacebookCredentials(token)
+        history.push(ACCOUNT)
+      })
+      .catch((error: any) =>
+        console.log(
+          `Error occurred while signing in using ${provider} provider.`,
+          error
+        )
+      )
+  }
+}
