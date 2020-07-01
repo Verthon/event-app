87cf9c2e295202e311ed3697ea8b3e6c1a3e6ea2
Verthon <krzysiekksordyl@gmail.com>
2020-07-01 10:52:02 +0200

    fix: removes infinite loop of useFetchCollection.

--- src/pages/Contact/Contact.styles.ts
+++ src/pages/Contact/Contact.styles.ts
@@ -9,10 +9,12 @@ const Logo = styled.img`
 const Title = styled.h1`
   margin-top: 0;
   color: ${({ theme }: Theme) => theme.fontWeights.bold};
+  padding: 0 1rem;
 `
 
 const Description = styled.p`
   color: ${({ theme }: Theme) => theme.colors.text};
+  padding: 0 1rem;
 `
 
 export const Styled = {
